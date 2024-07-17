47dc1ab6f58d7ca8ffade93e71b965afab829c6b
Valderrama20 <jose23122009@gmai.com>
2024-07-17 09:54:45 +0200

    Se cambio la libreria para escanear qr

--- frontend/src/components/QrScanner.tsx
+++ frontend/src/components/QrScanner.tsx
@@ -1,28 +1,18 @@
-import { QrReader } from "@blackbox-vision/react-qr-reader";
 import { useState } from "react";
+import { useZxing } from "react-zxing";
 
 const QrScanner: React.FC = () => {
-  const [qrData, setQrData] = useState<string | null>(null);
+  const [result, setResult] = useState<string>("");
 
-  const handleScan = (result: string | null) => {
-    if (result) setQrData(result);
-  };
-  const handleError = (error: any) => {
-    console.error(error);
-  };
+  const { ref } = useZxing({
+    onDecodeResult(result) {
+      setResult(result.getText());
+    },
+  });
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <div>
-        <QrReader
-          constraints={{ facingMode: "environment" }}
-          onResult={(result, error) => {
-            if (result) handleScan(result.getText());
-            if (error) handleError(error);
-          }}
-          className="h-[400px] w-[400px]"
-        />
-        <p>{qrData}</p>
-      </div>
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <video ref={ref} />
+      <p>{result}</p>
     </div>
   );
 };
