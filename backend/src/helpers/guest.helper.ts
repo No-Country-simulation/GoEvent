d724b456d4e288704ba342d21502bb8122dcf695
Tabare Bermudez <tabarebermudez@gmail.com>
2024-07-24 14:16:42 -0300

    perf: :bookmark: Replace xlsx library with node-xlsx

--- backend/src/helpers/guest.helper.ts
+++ backend/src/helpers/guest.helper.ts
@@ -1,14 +1,14 @@
-import xlsx from 'xlsx';
+import xlsx from 'node-xlsx';
 import IGuest from '../types/guest.types';
 
 export default class GuestHelper {
 
   // dictionary is a JSON used to map the excel columns to the database fields ex: {'fullname': 'nombre', 'email': 'correo_electronico'}
   static async excelImport(excelFile: any, dictionary: Record<string, string>): Promise<Partial<IGuest>[]> {
-    const workbook = xlsx.read(excelFile.buffer, { type: 'buffer' });
-    const firstSheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[firstSheetName];
-    const excelData = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
+    const workSheetsFromFile = xlsx.parse(excelFile.buffer);
+    const firstSheet = workSheetsFromFile[0];
+    const excelData = firstSheet.data;
+
     const columns: string[] = Array.isArray(excelData[0]) ? excelData[0] : [];
 
     const guests: Partial<IGuest>[] = excelData.slice(1).map((row: any) => {
