d1d01bb0a90826c3db396d8fa41f120a5a0c926a
Maidana <franco_maidana07@hotmail.com>
2024-07-31 00:30:17 -0300

    - Icono, navbar responsive y con pequeñas animaciones (hover).

--- frontend/src/components/home/UserMenu.tsx
+++ frontend/src/components/home/UserMenu.tsx
@@ -64,16 +64,17 @@ const UserMenu: React.FC<UserMenuProps> = ({ closeMenu }) => {
   ];
 
   return (
-    <div className="absolute flex h-[calc(100vh-60px)] w-full">
+    <div className="absolute flex min-h-[calc(100vh-60px)] w-full">
       {!isUserMenuOpen ? (
-        <div className="degradado flex h-full w-full flex-col lg:mx-20">
-          <div className="mx-10 flex justify-between pb-16 pt-5 text-2xl font-semibold">
+        <div className="degradado flex min-h-full w-full md:w-9/12 flex-col mx-auto">
+          <div className="mx-10 flex justify-between pb-8 pt-5 text-2xl font-semibold">
             <p>¡Hola {fullname}!</p>{" "}
-            <button onClick={() => closeMenu(false)}>
+            <button onClick={() => closeMenu(false)} className="p-1 rounded-full transition duration-300 ease-in-out hover:scale-[1.15]">
               <img src={closeIcon} alt="closeIcon" />
             </button>
           </div>
-          <ul className="flex-grow space-y-6 px-10">
+
+          <ul className="flex-grow space-y-2">
             {sections.map(
               ({
                 icon,
@@ -81,16 +82,16 @@ const UserMenu: React.FC<UserMenuProps> = ({ closeMenu }) => {
                 id,
                 extra = null,
                 mostrar = true,
-                onClick = () => {},
+                onClick = () => { },
               }) =>
                 mostrar && (
                   <li
                     key={id}
-                    className="flex cursor-pointer items-center space-x-7"
+                    className="flex cursor-pointer items-center space-x-7 pl-10 pr-4 py-4 transition ease duration-200 hover:bg-orange-400/50 hover:font-semibold"
                     onClick={onClick}
                   >
                     <img src={icon} alt={id} />
-                    <div className="flex flex-col">
+                    <div className="flex flex-col capitalize">
                       <span>{text}</span>
                       {extra && <span>{extra}</span>}
                     </div>
@@ -98,8 +99,8 @@ const UserMenu: React.FC<UserMenuProps> = ({ closeMenu }) => {
                 ),
             )}
           </ul>
-          <div className="m-16">
-            <button onClick={() => closeSesion(navigate, setUser)}>
+          <div className="m-16 text-end">
+            <button className="transition ease duration-200 px-6 py-2 rounded-2xl text-white font-semibold bg-red-500 hover:bg-red-600" onClick={() => closeSesion(navigate, setUser)}>
               Cerrar sesion
             </button>
           </div>
